1a776362e30fc47d005833dac2c3a847a144f37b
Pegwende Zabsore <pegwendezabsore@Pegwendes-MacBook-Air.local>
2022-01-30 12:37:00 -0600

    to do list list

---
+++ David_note/TodoList/index.js
@@ -0,0 +1,128 @@
+import { Appapi } from "./api.js";
+
+// ~~~~~~~~~~~~~~~view~~~~~~~~~~~~~~~~~~~~~
+const View = (() => {
+    const domstr = {
+        todolist: "#todolist__container",
+        deletebtn: ".delete_btn",
+        inputbox: ".todolist__input",
+    };
+    const render = (element, tmp) => {
+        element.innerHTML = tmp;
+    };
+    const createTmp = (arr) => {
+        let tmp = "";
+        arr.forEach((ele) => {
+            tmp += `
+                <li>
+                    <span>${ele.id} ${ele.title}</span>
+                    <button class="delete_btn" id="${ele.id}">
+                        X
+                    </button>
+                </li>
+            `;
+        });
+        return tmp;
+    };
+
+    return {
+        domstr,
+        render,
+        createTmp,
+    };
+})();
+
+// ~~~~~~~~~~~~~~~model~~~~~~~~~~~~~~~~~~~~~
+const Model = ((api, view) => {
+    class Todo {
+        constructor(title) {
+            this.userId = 20;
+            this.title = title;
+            this.completed = false;
+        }
+    }
+
+    class State {
+        #todolist = [];
+
+        get todolist() {
+            return this.#todolist;
+        }
+
+        set todolist(newdata) {
+            this.#todolist = newdata;
+
+            // render the todolist
+            const ele = document.querySelector(view.domstr.todolist);
+            const tmp = view.createTmp(this.#todolist);
+            view.render(ele, tmp);
+
+            // get all delete btns
+            // add eventlistener to each btn
+            // this.todolist = this.#todolist.filter((todo) => {
+            //     return +todo.id !== +event.target.id;
+            // });
+        }
+    }
+
+    const deleteTodo = api.deleteTodo;
+    const getTodos = api.getTodos;
+    const addTodo = api.addTodo;
+
+    return {
+        Todo,
+        State,
+        addTodo,
+        getTodos,
+        deleteTodo,
+    };
+})(Appapi, View);
+
+// ~~~~~~~~~~~~~~~controller~~~~~~~~~~~~~~~~~~~~~
+const Controller = ((model, view) => {
+    const state = new model.State();
+
+    const addTodo = () => {
+        const inputbox = document.querySelector(view.domstr.inputbox);
+
+        inputbox.addEventListener("keyup", (event) => {
+            if (event.key === "Enter") {
+                const todo = new model.Todo(event.target.value);
+                model.addTodo(todo).then((newtodo) => {
+                    state.todolist = [newtodo, ...state.todolist];
+                    event.target.value = "";
+                });
+            }
+        });
+    };
+
+    const deletTodo = () => {
+        const ele = document.querySelector(view.domstr.todolist);
+        ele.addEventListener("click", (event) => {
+
+            if (event.target.id !== '') {
+                state.todolist = state.todolist.filter((todo) => {
+                    return +todo.id !== +event.target.id;
+                });
+                console.log(event.target.id);
+                model.deleteTodo(event.target.id);
+            }
+        });
+    };
+
+    const init = () => {
+        model.getTodos().then((data) => {
+            state.todolist = data;
+        });
+    };
+
+    const bootstrap = () => {
+        init();
+        deletTodo();
+        addTodo();
+    };
+
+    return { bootstrap };
+})(Model, View);
+
+Controller.bootstrap();
--- Practice/index.js
+++ Practice/index.js
@@ -1,77 +1,159 @@
-window.addEventListener('load', ()=>{
+// window.addEventListener('load', ()=>{
     const theInput = document.querySelector(".search-input")
     const taskBody = document.querySelector(".task-body")
     const taskItem = document.querySelector(".task-item")
     const searchBtn = document.querySelector(".search-btn")
-    const toDelete = document.querySelector(".delete-btn")
+    // const toDelete = document.querySelector(".delete-btn")
     let inputValue=''
+
+// theInput.addEventListener("keyup",(event)=>{
+//     inputValue = theInput.value
+//     // console.log(inputValue)
+// } )
+
+// searchBtn.addEventListener('click', ()=>{
+//     if ( inputValue === ""){
+//         alert(" type something ")
+//         return;
+//     }
+
+//     const newDivItem = document.createElement("div")
+//     newDivItem.classList.add('task-item')
+
+//     const newElement = document.createElement('input')
+//     newElement.value = inputValue
+//     newElement.setAttribute("readonly","readonly")
+
+//     const newEdit = document.createElement('input')
+//     newEdit.value = "EDIT"
+//     newEdit.type = "submit"
+
+//     const newDelete = document.createElement('input')
+//     newDelete.value ="DELETE"
+//     newDelete.type ="submit"
+
+
+//     newElement.classList.add("input-item")
+//     newEdit.classList.add("edit-btn")
+//     newDelete.classList.add("delete-btn")
+
+//     newDivItem.append(newElement)
+//     newDivItem.append(newEdit)
+//     newDivItem.append(newDelete)
+    
+//     taskBody.append(newDivItem)
+
+//     inputValue=""
+//     theInput.value = ""
+
+//     newDelete.addEventListener('click', ()=>{
+//         taskBody.removeChild(newDivItem)
+//         console.log('deleteclicked')
+//      })
+
+//     newEdit.addEventListener('click', ()=>{
+//        let el = newEdit.value
+//        if ( el.toLowerCase() === "edit"){
+//         newElement.removeAttribute("readonly");
+//         newElement.focus();
+//         newEdit.value = "SAVE"
+//        } else{
+//         newElement.setAttribute('readonly', 'readonly')
+//         newEdit.value = "EDIT"
+//        }
+
+//     })
+// })
+
+
+
+
+
+function createTask(name){
+    // const taskBody = document.querySelector(".task-body")
+    const task=`
+            <div class="task-item">
+                <input class="input-item" type="text" value="${name}" readonly>
+                <input class="edit-btn" type="submit" value="EDIT">
+                <input  class="delete-btn" type="submit" value="DELETE"> 
+            </div>`
+
+            const position = "beforeend"
+
+    taskBody.insertAdjacentHTML(position, task)
+
+}
+
+
+
+const getData  =( () =>{
+     fetch("http://localhost:8000/tasks/")
+        .then(response => response.json())
+        .then(datas => {
+            datas.forEach((data)=>{
+                createTask(data.name)
+            })
+        })
+})()
+
+
 
 theInput.addEventListener("keyup",(event)=>{
     inputValue = theInput.value
-    // console.log(inputValue)
+    console.log(inputValue)
+
+    searchBtn.addEventListener('click', ()=>{
+        if ( inputValue === ""){
+            alert(" type something ")
+            return;
+        } else{
+            createTask(inputValue)
+            inputValue = ""
+        }
+    })
+
 } )
 
-searchBtn.addEventListener('click', ()=>{
-    if ( inputValue === ""){
-        alert(" type something ")
-        return;
+
+
+const postData =() =>{
+    let task = {
+        "name": "go to a restaurant at 12"
     }
 
-    const newDivItem = document.createElement("div")
-    newDivItem.classList.add('task-item')
+    fetch("http://localhost:8000/tasks", {
+        method: "POST",
+        body: JSON.stringify(task),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+        },
+    }).then((response) => response.json());
+}
 
-    const newElement = document.createElement('input')
-    newElement.value = inputValue
-    newElement.setAttribute("readonly","readonly")
-
-    const newEdit = document.createElement('input')
-    newEdit.value = "EDIT"
-    newEdit.type = "submit"
-
-    const newDelete = document.createElement('input')
-    newDelete.value ="DELETE"
-    newDelete.type ="submit"
+postData()
 
 
-    newElement.classList.add("input-item")
-    newEdit.classList.add("edit-btn")
-    newDelete.classList.add("delete-btn")
-
-    newDivItem.append(newElement)
-    newDivItem.append(newEdit)
-    newDivItem.append(newDelete)
-    
-    taskBody.append(newDivItem)
-
-    inputValue=""
-    theInput.value = ""
-
-    newDelete.addEventListener('click', ()=>{
-        taskBody.removeChild(newDivItem)
-        console.log('deleteclicked')
-     })
-
-    newEdit.addEventListener('click', ()=>{
-       let el = newEdit.value
-       if ( el.toLowerCase() === "edit"){
-        newElement.removeAttribute("readonly");
-        newElement.focus();
-        newEdit.value = "SAVE"
-       } else{
-        newElement.setAttribute('readonly', 'readonly')
-        newEdit.value = "EDIT"
-       }
-
+const deleteData = (id) =>{
+    fetch("http://localhost:8000/tasks/"+ id, {
+        method: "DELETE",
     })
+    .then((response) => response.text())
+    .then(res=>console.log(res))
+
+}
+
+
+
+taskBody.addEventListener("click", (event)=>{
+    let val = +event.target.id
+    deleteData(val)
+    console.log(val)
+})
+
+window.addEventListener("load", ()=>{
+    
 })
 
 
 
-})
-
-
-
-fetch("http://localhost:8000/list/")
- .then(response => response.json())
- .catch(error => console.error('Error:', error))
- .then(response => console.log('Success:', JSON.stringify(response)));
+// })
